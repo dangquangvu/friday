f96e320e1d55f5c7aa158ea198891bf4633a8a17
Vu Dang Quang <vu.dang@sotatek.com>
2020-12-16 04:52:27 +0700

    update user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './modules/auth/auth.module';
 import { BlogModule } from './modules/blog/blog.module';
 
 @Module({
-  imports: [BlogModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }),
+    AuthModule,
+    BlogModule,
+  ],
 })
 export class AppModule {}
--- src/modules/blog/blog.module.ts
+++ src/modules/blog/blog.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DbModel } from 'src/shared/constants';
+import { UserSchema } from '../auth/user.schema';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: DbModel.USER, schema: UserSchema }]),
+  ],
   controllers: [BlogController],
   providers: [BlogService],
 })
